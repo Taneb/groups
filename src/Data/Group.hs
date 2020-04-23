ed4312b81892f73704845c6a99adcd0fa8f50337
Jack Kelly <jack@jackkelly.name>
2020-04-24 09:06:27 +1000

    Port over instances defined in https://github.com/reflex-frp/patch

--- src/Data/Group.hs
+++ src/Data/Group.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE TypeOperators #-}
+
 module Data.Group where
 
+import Data.Functor.Const
+import Data.Functor.Identity
 import Data.Monoid
+import Data.Proxy
+import GHC.Generics
 
 -- |A 'Group' is a 'Monoid' plus a function, 'invert', such that:
 --
@@ -75,6 +81,33 @@ instance (Group a, Group b, Group c, Group d, Group e) => Group (a, b, c, d, e) 
   invert (a, b, c, d, e) = (invert a, invert b, invert c, invert d, invert e)
   pow (a, b, c, d, e) n = (pow a n, pow b n, pow c n, pow d n, pow e n)
 
+-- | Trivial group, Functor style.
+instance Group (Proxy x) where
+  invert _ = Proxy
+  _ ~~ _ = Proxy
+
+-- | 'Const' lifts groups into a functor.
+instance Group a => Group (Const a x) where
+  invert (Const a) = Const (invert a)
+  Const a ~~ Const b = Const (a ~~ b)
+
+-- | 'Identity' lifts groups pointwise (at only one point).
+instance Group a => Group (Identity a) where
+  invert (Identity a) = Identity (invert a)
+  Identity a ~~ Identity b = Identity (a ~~ b)
+
+-- | Product of groups, Functor style.
+instance (Group (f a), Group (g a)) => Group ((f :*: g) a) where
+  invert (a :*: b) = invert a :*: invert b
+  (a :*: b) ~~ (c :*: d) = (a ~~ c) :*: (b ~~ d)
+
+-- See https://gitlab.haskell.org/ghc/ghc/issues/11135#note_111802 for the reason Compose is not also provided.
+-- Base does not define Monoid (Compose f g a) so this is the best we can
+-- really do for functor composition.
+instance Group (f (g a)) => Group ((f :.: g) a) where
+  invert (Comp1 xs) = Comp1 (invert xs)
+  Comp1 xs ~~ Comp1 ys = Comp1 (xs ~~ ys)
+
 -- |An 'Abelian' group is a 'Group' that follows the rule:
 --
 -- @a \<> b == b \<> a@
@@ -97,6 +130,16 @@ instance (Abelian a, Abelian b, Abelian c) => Abelian (a, b, c)
 instance (Abelian a, Abelian b, Abelian c, Abelian d) => Abelian (a, b, c, d)
 
 instance (Abelian a, Abelian b, Abelian c, Abelian d, Abelian e) => Abelian (a, b, c, d, e)
+
+instance Abelian (Proxy x)
+
+instance Abelian a => Abelian (Const a x)
+
+instance Abelian a => Abelian (Identity a)
+
+instance (Abelian (f a), Abelian (g a)) => Abelian ((f :*: g) a)
+
+instance Abelian (f (g a)) => Abelian ((f :.: g) a)
 
 -- | A 'Group' G is 'Cyclic' if there exists an element x of G such that for all y in G, there exists an n, such that
 --
