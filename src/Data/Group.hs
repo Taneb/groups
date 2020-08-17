5a0fbd090fc9d846edb32b7aacd7d8a1857ec5fa
endgame <endgame@users.noreply.github.com>
2020-08-17 21:00:14 +1000

    Add features needed by `patch` library (#3)
    * Whitespace cleanup
    
    * Add group subtraction operator
    
    * Port over instances defined in https://github.com/reflex-frp/patch
    
    * Add some obvious Cyclic instances
    
    * CPP away some of the instances under old GHC and maintain compat
    
    Use `mappend` to support versions of base that doen't have Semigroup.
    
    * Also guard the Cyclic instances behind CPP
    
    * instance Group (Proxy x): Add explicit implementation of pow
    
    * Fix reference in LICENSE to sources from `patch`
    
    * Fix haddock
    
    * Comment nit
    
    * CPP away -XTypeOperators when not needed
    
    * Remove Obsidian Systems from LICENSE
    
    Ryan Trinkle (Partner, Obsidian Systems) consented to this:
    https://github.com/taneb/groups/pull/3#issuecomment-670782175

--- src/Data/Group.hs
+++ src/Data/Group.hs
@@ -1,14 +1,34 @@
+{-# LANGUAGE CPP           #-}
+#if MIN_VERSION_base(4,12,0)
+{-# LANGUAGE TypeOperators #-}
+#endif
+
 module Data.Group where
 
 import Data.Monoid
+#if MIN_VERSION_base(4,7,0)
+import Data.Proxy
+#endif
+#if MIN_VERSION_base(4,9,0)
+import Data.Functor.Const
+import Data.Functor.Identity
+#endif
+#if MIN_VERSION_base(4,12,0)
+import GHC.Generics
+#endif
 
--- |A 'Group' is a 'Monoid' plus a function, 'invert', such that: 
+-- |A 'Group' is a 'Monoid' plus a function, 'invert', such that:
 --
 -- @a \<> invert a == mempty@
 --
 -- @invert a \<> a == mempty@
 class Monoid m => Group m where
   invert :: m -> m
+
+  -- | Group subtraction: @x ~~ y == x \<> invert y@
+  (~~) :: m -> m -> m
+  x ~~ y = x `mappend` invert y
+
   -- |@'pow' a n == a \<> a \<> ... \<> a @
   --
   -- @ (n lots of a) @
@@ -20,7 +40,7 @@ class Monoid m => Group m where
     EQ -> mempty
     GT -> f x0 n0
     where
-      f x n 
+      f x n
         | even n = f (x `mappend` x) (n `quot` 2)
         | n == 1 = x
         | otherwise = g (x `mappend` x) (n `quot` 2) x
@@ -28,16 +48,18 @@ class Monoid m => Group m where
         | even n = g (x `mappend` x) (n `quot` 2) c
         | n == 1 = x `mappend` c
         | otherwise = g (x `mappend` x) (n `quot` 2) (x `mappend` c)
-  
+
+infixl 7 ~~
+
 instance Group () where
   invert () = ()
-  pow () _ = ()
+  pow _ _ = ()
 
 instance Num a => Group (Sum a) where
   invert = Sum . negate . getSum
   {-# INLINE invert #-}
   pow (Sum a) b = Sum (a * fromIntegral b)
-  
+
 instance Fractional a => Group (Product a) where
   invert = Product . recip . getProduct
   {-# INLINE invert #-}
@@ -55,7 +77,7 @@ instance Group b => Group (a -> b) where
 instance (Group a, Group b) => Group (a, b) where
   invert (a, b) = (invert a, invert b)
   pow (a, b) n = (pow a n, pow b n)
-  
+
 instance (Group a, Group b, Group c) => Group (a, b, c) where
   invert (a, b, c) = (invert a, invert b, invert c)
   pow (a, b, c) n = (pow a n, pow b n, pow c n)
@@ -68,8 +90,9 @@ instance (Group a, Group b, Group c, Group d, Group e) => Group (a, b, c, d, e) 
   invert (a, b, c, d, e) = (invert a, invert b, invert c, invert d, invert e)
   pow (a, b, c, d, e) n = (pow a n, pow b n, pow c n, pow d n, pow e n)
 
+
 -- |An 'Abelian' group is a 'Group' that follows the rule:
--- 
+--
 -- @a \<> b == b \<> a@
 class Group g => Abelian g
 
@@ -91,6 +114,7 @@ instance (Abelian a, Abelian b, Abelian c, Abelian d) => Abelian (a, b, c, d)
 
 instance (Abelian a, Abelian b, Abelian c, Abelian d, Abelian e) => Abelian (a, b, c, d, e)
 
+
 -- | A 'Group' G is 'Cyclic' if there exists an element x of G such that for all y in G, there exists an n, such that
 --
 -- @y = pow x n@
@@ -101,3 +125,63 @@ generated :: Cyclic a => [a]
 generated =
   iterate (mappend generator) mempty
 
+instance Cyclic () where
+  generator = ()
+
+#if MIN_VERSION_base(4,7,0)
+-- | Trivial group, Functor style.
+instance Group (Proxy x) where
+  invert _ = Proxy
+  _ ~~ _ = Proxy
+  pow _ _ = Proxy
+
+instance Abelian (Proxy x)
+
+instance Cyclic (Proxy x) where
+  generator = Proxy
+#endif
+
+-- 'Const' has existed for a long time, but the Monoid instance
+-- arrives in base-4.9.0.0. Similarly, 'Identity' was defined in
+-- base-4.8.0.0 but doesn't get the Monoid instance until base-4.9.0.0
+#if MIN_VERSION_base(4,9,0)
+-- | 'Const' lifts groups into a functor.
+instance Group a => Group (Const a x) where
+  invert (Const a) = Const (invert a)
+  Const a ~~ Const b = Const (a ~~ b)
+
+-- | 'Identity' lifts groups pointwise (at only one point).
+instance Group a => Group (Identity a) where
+  invert (Identity a) = Identity (invert a)
+  Identity a ~~ Identity b = Identity (a ~~ b)
+
+instance Abelian a => Abelian (Const a x)
+
+instance Abelian a => Abelian (Identity a)
+
+instance Cyclic a => Cyclic (Const a x) where
+  generator = Const generator
+
+instance Cyclic a => Cyclic (Identity a) where
+  generator = Identity generator
+#endif
+
+-- (:*:) and (:.:) exist since base-4.6.0.0 but the Monoid instances
+-- arrive in base-4.12.0.0.
+#if MIN_VERSION_base(4,12,0)
+-- | Product of groups, Functor style.
+instance (Group (f a), Group (g a)) => Group ((f :*: g) a) where
+  invert (a :*: b) = invert a :*: invert b
+  (a :*: b) ~~ (c :*: d) = (a ~~ c) :*: (b ~~ d)
+
+-- See https://gitlab.haskell.org/ghc/ghc/issues/11135#note_111802 for the reason Compose is not also provided.
+-- Base does not define Monoid (Compose f g a) so this is the best we can
+-- really do for functor composition.
+instance Group (f (g a)) => Group ((f :.: g) a) where
+  invert (Comp1 xs) = Comp1 (invert xs)
+  Comp1 xs ~~ Comp1 ys = Comp1 (xs ~~ ys)
+
+instance (Abelian (f a), Abelian (g a)) => Abelian ((f :*: g) a)
+
+instance Abelian (f (g a)) => Abelian ((f :.: g) a)
+#endif
